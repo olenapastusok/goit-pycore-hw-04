9780e8c168fbd17e18a0ec714dcfe0323d4927c1
olenapastusok <olenapastusok@gmail.com>
2025-10-29 01:03:42 +0200

    Exercise 3

---
+++ Exercise 3/visualization.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+from colorama import Fore, init
+
+init(autoreset=True)
+
+def directory_visualization (path):
+
+    try:
+        for item in sorted(path.iterdir()):
+            if item.is_dir():
+                print(Fore.BLUE + item.name + "/")
+                directory_visualization(item)
+            else:
+                print("|____" + Fore.GREEN+item.name)
+    except PermissionError:
+        print(Fore.RED + "You do not have permission to directory" + str(path))
+    except FileNotFoundError:
+        print(Fore.RED + "File is not found" + str(path))
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 visualization.py <directory_path>")
+
+root_path = Path(sys.argv[1])
+print(Fore.YELLOW + "Directory structure: \n")
+directory_visualization(root_path)
